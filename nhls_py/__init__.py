e58f19692a04138d4c1778c64c695dbbf90bc352
RussellBentley <russell.w.bentley@icloud.com>
2025-06-25 16:51:54 -0400

    Handle ruff lints

--- nhls_py/__init__.py
+++ nhls_py/__init__.py
@@ -1,5 +1,6 @@
+from .session_init import session_init as session_init
+
 # Module level variable to store the location 
 # nhls checkout / build artifacts
 nhls_path = "~/.cache/nhls"
 
-from .session_init import session_init
